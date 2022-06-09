127ec76be99f6624cb0e7c08b170591ffcbd2d32
Luis Carlos Isaza Machado <luis.isaza.0ds@utap.edu.co>
2022-06-08 22:17:09 -0500

    fix: Solucionando error funciones saludo

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+from ast import Return
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return "Adios codigo"
+
+@app.route('/Hola')
+def greating():
     return "Hola Mundo"
